01d24f88f576c41b50f81d6c4674074d186b833e
MdAfsarHossain <afsar291722@gmail.com>
2025-10-05 20:49:03 +0600

    Auth controller ts file added

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signUp(@Body() body: { email: string; password: string }) {
+    return this.authService.signUp(body.email, body.password);
+  }
+
+  @Post('login')
+  login(@Body() body: { email: string; password: string }) {
+    return this.authService.login(body.email, body.password);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
